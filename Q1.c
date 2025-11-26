b5757615e365f036a79350a566a2f60a45160a8e
harshvaland-del <er.harsh26@gmail.com>
2025-11-25 23:48:53 -0800

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    printf("The sum of all numbers is: %d\n", sum);
+}
